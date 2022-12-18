5aef123f3197537598b45bf93531b622ad4b1620
Aditya <77727680+AdityaMotale@users.noreply.github.com>
2022-12-18 18:27:15 +0530

    modified gitignore

--- atleave_api/src/core/keys.ts
+++
@@ -1,4 +0,0 @@
-export const DB_KEY: string =
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InF5amV6YmtuYWVvaWFxY2lrbW9zIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzA0Mjk4MzUsImV4cCI6MTk4NjAwNTgzNX0.YLzIpRgDsRwcnVW8qUzSOVOyWl_jN3LO8I-ovgp-dC0';
-
-export const DB_URL: string = 'https://qyjezbknaeoiaqcikmos.supabase.co';
