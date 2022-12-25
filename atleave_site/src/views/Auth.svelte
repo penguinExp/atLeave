8c49f371f1e118f02e543b81855371b854e86e2b
Aditya <77727680+AdityaMotale@users.noreply.github.com>
2022-12-25 22:53:11 +0530

    impl of auth system

---
+++ atleave_site/src/views/Auth.svelte
@@ -0,0 +1,157 @@
+<script>
+  let email, passwd;
+
+  let isExists = false;
+
+  let isLoading = false;
+
+  let hasError = false;
+
+  async function postCall(d) {
+    isLoading = true;
+    let headersList = {
+      Accept: "*/*",
+      "Content-Type": "application/json",
+    };
+    const res = await fetch("http://localhost:8888/auth", {
+      body: d,
+      mode: "cors",
+      method: "POST",
+      headers: headersList,
+    })
+      .then((res) => res.json())
+      .then((res) => console.log(res));
+  }
+
+  async function loginCall() {
+    passwd = element?.value;
+
+    console.log("Login call");
+
+    if (!isLoading) {
+      const res = await fetch(
+        `http://localhost:8888/auth?email=${email}&passwd=${passwd}`
+      )
+        .then((res) => res.json())
+        .then((data) => {
+          return data;
+        });
+
+      if (res["data"]) {
+        hasError = false;
+        window.location.assign("http://127.0.0.1:3000/home");
+      } else {
+        hasError = true;
+      }
+    }
+  }
+
+  async function signUpCall() {
+    passwd = element?.value;
+
+    if (!isLoading) {
+      let res = await postCall(
+        JSON.stringify({ email: email, passwd: passwd })
+      );
+
+      if (res["statusCode"] == 201) {
+        window.location.assign("http://127.0.0.1:3000/home");
+      }
+    }
+  }
+
+  async function checkEmail() {
+    const element = document.getElementById("input");
+
+    if (!email) {
+      email = element?.value;
+
+      isLoading = true;
+
+      const response = await fetch(
+        `http://localhost:8888/auth/email?email=${email}`
+      )
+        .then((res) => res.json())
+        .then((data) => {
+          return data;
+        });
+
+      if (response["statusCode"] == 200) {
+        isExists = true;
+      } else {
+        isExists = false;
+      }
+
+      isLoading = false;
+
+      element.value = "";
+      element.placeholder = "Enter your password";
+    }
+  }
+</script>
+
+<section class="main">
+  <div class="inputSec">
+    <input type="email" placeholder="Enter your email" id="input" />
+    {#if hasError}
+      <p id="errorText">Please enter a correct password</p>
+    {/if}
+  </div>
+  <input
+    type="button"
+    value={isLoading ? "...Loading" : "Continue"}
+    id="button"
+    on:click={!passwd ? checkEmail() : isExists ? loginCall() : signUpCall()}
+  />
+</section>
+
+<style>
+  .main {
+    height: 100%;
+    margin-left: 60px;
+    margin-right: 60px;
+    display: flex;
+    flex-flow: column nowrap;
+    justify-content: center;
+    gap: 40px;
+  }
+
+  .main > .inputSec {
+    width: 100%;
+    display: flex;
+    flex-flow: column nowrap;
+    justify-content: center;
+    align-items: flex-start;
+  }
+
+  .main > .inputSec > p {
+    color: red;
+    font-size: 14px;
+    font-weight: 300;
+    padding-left: 8px;
+    margin-top: 16px;
+  }
+
+  input {
+    width: 100%;
+    height: 45px;
+    border-radius: 12px;
+    outline: none;
+    background-color: #1e2435;
+    border: none;
+    color: white;
+    padding: 0px 10px;
+    font-family: "Rubik";
+    font-size: 14px;
+  }
+
+  #button {
+    height: 45px;
+    cursor: pointer;
+    background-color: #a259ff;
+    color: white;
+    border-radius: 8px;
+    font-size: 18px;
+    font-weight: 300;
+  }
+</style>
